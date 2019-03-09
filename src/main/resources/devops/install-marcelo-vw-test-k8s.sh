3a3852e45bc6e33bb76a287d6e61a858a2c9a88b
Marcelo Pereira <marcelovcpereira@gmail.com>
2019-03-09 14:50:20 +0100

    fix

--- src/main/resources/devops/install-marcelo-vw-test-k8s.sh
+++ src/main/resources/devops/install-marcelo-vw-test-k8s.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+###########################################
+#       INSTALLATION WITH KUBERNETES      #
+###########################################
+sudo snap install docker
+sudo snap install microk8s --classic
+sudo snap install kubectl --classic
+sudo echo "export PATH=/snap/bin:$PATH" >> /etc/profile && source /etc/profile
