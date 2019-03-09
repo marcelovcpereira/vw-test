b564529fbcac82fbd37571083e37c95a9345a400
Marcelo Pereira <marcelovcpereira@gmail.com>
2019-03-09 11:55:45 +0100

    refactors Dockerfile

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # vw-test
+
+This project is the solution for the Volkswagen test challenge
+
+This project is composed of 2 containers:
+
+Marcelo Mysql Container, that runs an empty mysql image to persist measures data.
+Marcelo VW Test Container, that runs a SpringBoot application that reads measures information from UCI repository (http://archive.ics.uci.edu/ml/datasets/air+quality)
+as a CSV file. Then it parses the file, validates the rows and saves all measures in the Mysql instance.
+
+The Marcelo VW Test Container also exposes a webserver for showing data from the database.
+
+
+
+
--- src/main/resources/devops/run-marcelo-vw-test.sh
+++ src/main/resources/devops/run-marcelo-vw-test.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-snap install microk8s --classic
-snap install kubectl
-snap install docker
-echo "export PATH=/snap/bin:$PATH" >> /etc/profile && source /etc/profile
-
-docker build -t marcelo-vw-test:latest ./docker
 
 
-kubectl apply -f k8s/deployment.yaml
-kubectl apply -f k8s/service.yaml
-kubectl port-forward svc/marcelo-vw-test 8080:8080
+sudo snap install docker
+sudo docker build -t marcelo-vw-test:latest ./docker
+sudo docker-compose -f ./docker/docker-compose.yml up
+
+###########################################
+#In the future we could improve to use K8S#
+###########################################
+#sudo snap install microk8s --classic
+#sudo snap install kubectl
+#echo "export PATH=/snap/bin:$PATH" >> /etc/profile && source /etc/profile
+#kubectl apply -f k8s/deployment.yaml
+#kubectl apply -f k8s/service.yaml
+#kubectl port-forward svc/marcelo-vw-test 8080:8080
