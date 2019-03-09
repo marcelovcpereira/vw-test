324b0a6a0dc7b3e28276361507c8d1978bac4ca9
Marcelo Pereira <marcelovcpereira@gmail.com>
2019-03-09 13:28:42 +0100

    new installation

--- src/main/resources/devops/run-marcelo-vw-test.sh
+++ src/main/resources/devops/run-marcelo-vw-test.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 
-sudo snap install docker
+apt-get update
+apt-get install sudo -y
+
+sudo apt-get remove docker docker-engine docker.io lxc-docker
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+#snap install docker
 sudo docker build -t marcelo-vw-test:latest ./docker
 sudo docker-compose -f ./docker/docker-compose.yml up
 
@@ -14,3 +36,5 @@ sudo docker-compose -f ./docker/docker-compose.yml up
 #kubectl apply -f k8s/deployment.yaml
 #kubectl apply -f k8s/service.yaml
 #kubectl port-forward svc/marcelo-vw-test 8080:8080
+
+#tar -xvzf marcelo-vw-test.tar.gz
