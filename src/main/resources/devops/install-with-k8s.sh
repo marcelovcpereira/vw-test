0e29a2a9635eb4246c0ff4c20478ab8a9fd86954
Marcelo Pereira <marcelovcpereira@gmail.com>
2019-03-09 14:46:05 +0100

    k8s fix

--- src/main/resources/devops/install-with-k8s.sh
+++ src/main/resources/devops/install-with-k8s.sh
@@ -2,3 +2,6 @@
 git clone https://github.com/marcelovcpereira/vw-test
 cd vw-test/src/main/resources/devops
 sh install-marcelo-vw-test-k8s.sh
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
+kubectl port-forward svc/marcelo-vw-test 8888:8080
